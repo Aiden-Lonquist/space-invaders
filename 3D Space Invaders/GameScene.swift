8b27017343cab702dfcf72c85587da4fd21263b7
Jacky Lau <jlau154@se12l0325-17.ad.bcit.ca>
2022-02-18 20:24:58 -0800

    added moving functionality for player

--- 3D Space Invaders/GameScene.swift
+++ 3D Space Invaders/GameScene.swift
@@ -9,6 +9,25 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    let player = SKSpriteNode(imageNamed: "ship")
+    
+    var gameArea: CGRect
+    
+    override init(size: CGSize) {
+        
+        let maxAspectRatio: CGFloat = 16.0/9.0
+        let playableWidth = size.height / maxAspectRatio
+        let margin = (size.width - playableWidth) / 2
+        gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+        
+        super.init(size: size)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder) has not been implemented")
+    }
     override func didMove(to view: SKView) {
         
         let background = SKSpriteNode(imageNamed: "background")
@@ -17,9 +36,26 @@ class GameScene: SKScene {
         background.zPosition = 0
         self.addChild(background)
         
-        let player = SKSpriteNode(imageNamed: "player")
         player.setScale(1)
         player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
         self.addChild(player)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch: AnyObject in touches {
+            let pointOfTouch = touch.location(in: self)
+            let previousPointOfTouch = touch.previousLocation(in: self)
+            
+            let amountDragged = pointOfTouch.x - previousPointOfTouch.x
+            
+            player.position.x += amountDragged
+            
+            if player.position.x > gameArea.maxX {
+                player.position.x = gameArea.maxX
+            }
+            if player.position.x < gameArea.minX {
+                player.position.x = gameArea.minX
+            }
+        }
     }
 }
